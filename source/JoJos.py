a1ff30982b400d8edaebdc2b94d76bdc0d668e21
KacperNapierski <kacper.t.napierski@gmail.com>
2023-12-14 20:31:25 +0100

    basic_stats update working

---
+++ source/JoJos.py
@@ -0,0 +1,75 @@
+import pygame
+from pygame.sprite import Group
+from settings import *
+
+#TODO
+#[ ] hides all variables of classes inside functions
+#[ ] create a group and operate only on functions
+
+class JoJo(pygame.sprite.Sprite):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.cleanliness = 100
+        self.hunger = 100
+        self.happiness = 100
+        self.money_generation_rate = 1
+        self.physiological_need = 0.0
+
+
+    def play(self):
+        ...
+    
+    def pet(self):
+            #TODO 
+            #[ ]animation
+            #[ ] Jotaro anoyence -> happiness lower
+            #[ ]limit usage
+        if self.happiness < 98: self.happiness += 2
+        
+        else: ...
+
+    def poop():
+        ...
+        
+    
+    def apply_needs(self):
+        self.hunger -= 3
+        self.happiness -= 5
+        #TODO when poop exist change for poop
+        self.physiological_need += 0.01
+        if self.physiological_need == 1:
+            self.cleanliness -= 10
+        else: self.cleanliness -=2
+
+
+    def animation_state():
+        ...
+
+    def update(self):
+        #self.apply_needs()
+        self.play()
+        #self.animation_state()
+
+
+
+class Jotaro(JoJo):
+    def __init__(self) -> None:
+        super().__init__()
+        name_font = pygame.font.Font(None,50)
+        self.name = name_font.render('Jotaro', False, "green")
+        self.jotaro = self.name.get_rect(center = (WIDTH//2, HEIGHT//4))
+
+
+
+class Giorno(JoJo):
+    def __init__(self) -> None:
+        super().__init__()
+        self.money_generation_rate = 1.3
+
+
+
+class Kakyoin(JoJo):
+    def __init__(self) -> None:
+        super().__init__()
+
---
+++ source/main.py
@@ -0,0 +1,54 @@
+import pygame, sys
+from settings import *
+from JoJos import *
+
+
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        pygame.display.set_caption("JoJoGotchi")
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.game_active = True
+        self.character_update_timer = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.character_update_timer, 5000)
+
+    def run(self):
+        #TODO temp declaration
+        jojo = Jotaro()
+
+        while True:
+            #TODO event handler class?
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+                if self.game_active == True:
+                    if event.type == self.character_update_timer:
+                        jojo.apply_needs()
+
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if jojo.jotaro.collidepoint(event.pos):
+                            jojo.pet()
+            
+
+            if self.game_active:
+        
+                pygame.draw.rect(self.screen,'#c0e8ec',jojo.jotaro)
+                self.screen.blit(jojo.name, jojo.jotaro)
+                jojo.update()
+                print(f'hunger: {jojo.hunger}')
+                print(f'happiness {jojo.happiness}')
+                print(f'clean {jojo.cleanliness}')
+                print(f'poop {jojo.physiological_need}')
+                print(f'#######')
+        
+    
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
